a2413db5c20b6e294027977880147a178d9edffd
Luis Fernando <Luis Fernando da Silva>
2018-12-14 16:03:17 -0200

    add getPeriod

---
+++ tests/getPeriod.test.js
@@ -0,0 +1,28 @@
+const getPeriod = require('../lib/getPeriod');
+
+describe('getPeriod', () => {
+  it('sould return howMany & unit to valid values', () => {
+    const validValues = [
+      '2hours',
+      '20minutes',
+      '15days',
+      '1month'
+    ];
+    const expectValues = [
+      { howMany: 2, unit: 'hours' },
+      { howMany: 20, unit: 'minutes' },
+      { howMany: 15, unit: 'days' },
+      { howMany: 1, unit: 'month' }
+    ];
+    validValues.forEach((value, i) => expect(getPeriod(value)).toEqual(expectValues[i]));
+  });
+
+  it('should return undefined passing invalid data type', () => {
+    const invalidData = [
+      ['sdaa'],
+      { saf: 'dasf' },
+      445646
+    ];
+    invalidData.forEach(data => expect(getPeriod(data)).toBe(undefined));
+  });
+});
