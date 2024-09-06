e557d3e8f4d0eae321c0d9d01e133fb7b6c61ef3
alemao <brunodiasdev@gmail.com>
2024-09-06 03:06:44 -0300

    fix: set as optional the use of translated labels on healthScore

--- tests/healthScore.test.js
+++ tests/healthScore.test.js
@@ -56,27 +56,27 @@ describe('healthScore', () => {
 
   describe('getTranslatedChurnStatusLabel', () => {
     it('should return Muito Baixa for churn 0', () => {
-      expect(getTranslatedChurnStatusLabel(0)).toBe(CHURN.LABELS.SUPER_LOW);
+      expect(getTranslatedChurnStatusLabel(0, false)).toBe(CHURN.LABELS.SUPER_LOW);
     });
 
     it('should return Baixa for churn 20', () => {
-      expect(getTranslatedChurnStatusLabel(20)).toBe(CHURN.LABELS.LOW);
+      expect(getTranslatedChurnStatusLabel(20, false)).toBe(CHURN.LABELS.LOW);
     });
 
     it('should return Média for churn 40', () => {
-      expect(getTranslatedChurnStatusLabel(40)).toBe(CHURN.LABELS.MEDIUM);
+      expect(getTranslatedChurnStatusLabel(40, false)).toBe(CHURN.LABELS.MEDIUM);
     });
 
     it('should return Alta for churn 60', () => {
-      expect(getTranslatedChurnStatusLabel(60)).toBe(CHURN.LABELS.HIGH);
+      expect(getTranslatedChurnStatusLabel(60, false)).toBe(CHURN.LABELS.HIGH);
     });
 
     it('should return Muito Alta for churn 80', () => {
-      expect(getTranslatedChurnStatusLabel(80)).toBe(CHURN.LABELS.SUPER_HIGH);
+      expect(getTranslatedChurnStatusLabel(80, false)).toBe(CHURN.LABELS.SUPER_HIGH);
     });
 
     it('should return Invalid value for churn -1', () => {
-      expect(getTranslatedChurnStatusLabel(-1)).toBe('Invalid value');
+      expect(getTranslatedChurnStatusLabel(-1, false)).toBe('Invalid value');
     });
   });
 });
